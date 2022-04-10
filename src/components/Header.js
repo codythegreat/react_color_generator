d56b1eb50a907a6adaf5e5069cfac9934febf638
codythegreat <maxiecody@gmail.com>
2022-04-10 07:56:22 -0500

    adds subtext to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = (props) => {
     return (
         <div>
             <h1 style={{fontSize: 24}}>{props.text}</h1>  
+            <p style={{fontSize: 12}}>{props.subtext}</p>  
         </div>
     )
 }
